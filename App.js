e2be4f03e9cd97033c4be7d50bdb22df4895a349
Brian López <brianlama1220@gmail.com>
2020-12-01 17:43:26 -0400

    first commit

--- App.js
+++ App.js
@@ -30,69 +30,32 @@ const signal = () => {
     .catch((error) => {
       console.error(error);
     });
-// //////////////// #3
-//   const data = { location: "value" };
-//   const options = {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(data),
-//   };
-//   fetch("http://10.0.0.55:3500/", options)
-//     .then((resp) => resp.json())
-//     .then((data) => {
-//       () => console.log(data);
-//     })
-//     .catch((err) => {
-//       console.log("Error : " + err);
-//     });
+  // //////////////// #3
+  //   const data = { location: "value" };
+  //   const options = {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(data),
+  //   };
+  //   fetch("http://10.0.0.55:3500/", options)
+  //     .then((resp) => resp.json())
+  //     .then((data) => {
+  //       () => console.log(data);
+  //     })
+  //     .catch((err) => {
+  //       console.log("Error : " + err);
+  //     });
 };
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text style={styles.title}>
-          The title and onPress handler are required. It is recommended to set
-          accessibilityLabel to help make your app usable by everyone.
-        </Text>
-        <Button
-          title="Press me"
-          onPress={() => Alert.alert("Simple Button pressed")}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-          Adjust the color in a way that looks standard on each platform. On
-          iOS, the color prop controls the color of the text. On Android, the
-          color adjusts the background color of the button.
-        </Text>
-        <Button
-          title="Press me"
-          color="#f194ff"
-          onPress={() => Alert.alert("Button with adjusted color pressed")}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-          All interaction for the component are disabled.
-        </Text>
-        <Button
-          title="Press me"
-          disabled
-          onPress={() => Alert.alert("Cannot press this one")}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-          This layout strategy lets the title define the width of the button.
-        </Text>
+        <Text style={styles.title}></Text>
         <View style={styles.fixToText}>
-          <Button title="Dale Aqui" onPress={() => signal()} />
+          <Button title="Presione para cruzar" onPress={() => signal()} />
         </View>
       </View>
     </SafeAreaView>
@@ -112,6 +75,8 @@ const styles = StyleSheet.create({
   fixToText: {
     flexDirection: "row",
     justifyContent: "space-between",
+    justifyContent: "center",
+    alignItems: "center",
   },
   separator: {
     marginVertical: 8,
